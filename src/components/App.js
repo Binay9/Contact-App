2d80fd3d1587d6569c41bbd4df074e4936d09f41
Binay9 <soundofpeace.bd@gmail.com>
2022-04-11 00:59:39 +0100

    api using axois & json-server

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Routes, Route, } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
+import api from '../api/contacts';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -9,14 +10,30 @@ import ContactDetail from './ContactDetail';
 
 function App() {
 
-  const LOCAL_STORAGE_KEY = "contacts";
+  // const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    setContacts([...contacts, { id: uuid(), ...contact }]);
+  //RetrieveContacts
+  const retriveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  }
+
+  //AddContact
+  const addContactHandler = async (contact) => {
+
+    const request = {
+      id: uuid(),
+      ...contact,
+    };
+
+    const response = await api.post("/contacts", request);
+    setContacts([...contacts, response.data]);
   };
 
-  const removeContactHandler = (id) => {
+  //DeleteContact
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
     });
@@ -26,15 +43,18 @@ function App() {
 
   useEffect(
     () => {
-      const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-      if (retriveContacts) setContacts(retriveContacts);
-    },
-    [],
-  );
+      // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      // if (retriveContacts) setContacts(retriveContacts);
+      const getAllContacts = async () => {
+        const allContacts = await retriveContacts();
+        if(allContacts) setContacts(allContacts);
+      };
+      getAllContacts();
+    },[]);
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
 
   return (
     <div className="myMain">
